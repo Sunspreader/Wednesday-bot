7f579f47f71f663b9f6b72e7bd76063fad6e0a0b
Konstantin <gerb200@gmail.com>
2023-02-21 16:13:15 +0300

    fix errors

--- events/ai.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-curl -F "chat_id="1172957592 -F "video=@/home/sunspreader/Pictures/jaba.gif" \
-https://api.telegram.org/bot6292843238:AAHWD2JGk7f32zrq3oeh3eqh45GfwzFTDGM/sendVideo
--- events/regular.sh
+++ events/regular.sh
@@ -2,10 +2,9 @@
 
 source ./source/fact_container.sh
 source ./source/source.sh
+source ./source/debug.sh
 
-bash ./source/debug.sh
+curl -X POST https://api.telegram.org/bot*your bot token*/sendMessage -d chat_id=*your chat id* -d text="Жабьи факты: ${FACT_ARRAY[${RANDOM_FACT}]}" | json_pp -json_opt pretty,canonical >> ../log/log.txt
 
-curl -X POST https://api.telegram.org/bot6292843238:AAHWD2JGk7f32zrq3oeh3eqh45GfwzFTDGM/sendMessage -d chat_id=1172957592 -d text="Жабьи факты: ${FACT_ARRAY[${RANDOM_FACT}]}" | json_pp -json_opt pretty,canonical >> ../log/log.txt
-
-curl -F "chat_id="1172957592 -F "photo=@../media/regular/"${RANDOM_PICTURE}"" \
-https://api.telegram.org/bot6292843238:AAHWD2JGk7f32zrq3oeh3eqh45GfwzFTDGM/sendphoto | json_pp -json_opt pretty,canonical >> ../log/log.txt
+curl -F "chat_id="*your chat id* -F "photo=@../media/regular/"${RANDOM_PICTURE}"" \
+https://api.telegram.org/bot*your bot token*/sendphoto | json_pp -json_opt pretty,canonical >> ../log/log.txt
